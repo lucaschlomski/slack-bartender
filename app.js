52e598e49eacf3d4ba69868ae6a20b627e1e554b
Luca Schlomski <luca.schlomski@pickware.de>
2022-07-12 19:57:10 +0200

    barbot v2.0.0

---
+++ app.js
@@ -0,0 +1,135 @@
+require('dotenv').config()
+require('./messsage')
+
+const { App, AwsLambdaReceiver } = require('@slack/bolt')
+const cron = require("cron").CronJob
+const bar = require('./bar')
+
+
+// initializes the app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+})
+
+//channel to post message; random = C48FHRR8E ; test-luca = GQVLQ43A8 ; general = C4745E0QG
+const channel = {
+  random: "C48FHRR8E",
+  gerneral: "C4745E0QG",
+  testLuca: "GQVLQ43A8"
+}
+
+// listen for slack command /openbar and determine barkeeper
+app.command("/openbar", async ({ack, command}) => {
+  ack()
+  await send_message(command.user_id)
+  console.log(command.user_name + " opened the bar via a command")
+})
+
+// send message
+async function send_message(barkeeper) {
+  app.client.chat.postMessage({
+    channel: channel.testLuca,
+    text: "bar selection message",
+    blocks: barMessage
+  })
+}
+
+// update message
+async function update_message(message, message_channel, message_ts) {
+  app.client.chat.update({
+    channel: message_channel,
+    ts: message_ts,
+    text: "bar selection message",
+    blocks: message
+  })
+}
+
+// button to become barkeeper (not in use because no active cron)  
+app.action("barkeeper_button", async ({ack, body}) => {
+  await ack()
+  console.log(body.user.name + " ist barkeeper")
+  body.message.blocks[1] = {
+		type: "section",
+		text: {
+			type: "mrkdwn",
+			text: `:party_wizard: <@${body.user.name}>`
+		}
+	}
+  update_message(body.message.blocks, body.container.channel_id, body.container.message_ts)
+})
+
+// Augustiner
+app.action("button_1", async ({ack, body}) => {
+  await ack()
+  console.log("1x Option_1 für " + body.user.name)
+  body.message.blocks[5].elements[0].text += ` <@${body.user.id}>`
+  update_message(body.message.blocks, body.container.channel_id, body.container.message_ts)
+})
+
+// Grohe
+app.action("button_2", async ({ack, body}) => {
+  await ack()
+  console.log("1x Option_2 für " + body.user.name)
+  body.message.blocks[7].elements[0].text += ` <@${body.user.id}>`
+  update_message(body.message.blocks, body.container.channel_id, body.container.message_ts)
+})
+
+// Radler
+app.action("button_3", async ({ack, body}) => {
+  await ack()
+  console.log("1x Option_3 für " + body.user.name)
+  body.message.blocks[9].elements[0].text += ` <@${body.user.id}>`
+  update_message(body.message.blocks, body.container.channel_id, body.container.message_ts)
+})
+
+// Aperol
+app.action("button_4", async ({ack, body}) => {
+  await ack()
+  console.log("1x Option_4 für " + body.user.name);
+  body.message.blocks[11].elements[0].text += ` <@${body.user.id}>`
+  update_message(body.message.blocks, body.container.channel_id, body.container.message_ts)
+})
+
+// Melone
+app.action("button_5", async ({ack, body}) => {
+  await ack()
+  console.log("1x Option_5 für " + body.user.name)
+  body.message.blocks[13].elements[0].text += ` <@${body.user.id}>`
+  update_message(body.message.blocks, body.container.channel_id, body.container.message_ts)
+})
+
+// Jägermeister
+app.action("button_6", async ({ack, body}) => {
+  await ack()
+  console.log("1x Option_6 für " + body.user.name)
+  body.message.blocks[15].elements[0].text += ` <@${body.user.id}>`
+  update_message(body.message.blocks, body.container.channel_id, body.container.message_ts)
+})
+
+// open pop-up Extrawünsche
+app.action("extra_button", async ({ack, body}) => {
+  await ack()
+  app.client.views.open({
+    token: token,
+    trigger_id: body.trigger_id,
+    view: barModal
+  })
+})
+
+// Extrawünsche
+app.view('view_1', async ({ ack, body, view}) => {
+  await ack()
+  body.message.blocks[18].elements[0].text += `>*${view.state.values.block_1.input_1.value}* für <@${body.user.id}>\n`
+  update_message(body.message.blocks, body.container.channel_id, body.container.message_ts)
+  console.log("extrawunsch für " + body.user.name)
+})
+
+// starting the app
+async function startApp() {
+  await app.start(process.env.PORT || 3000)
+  console.log('⚡️ slack-bartender app is running!')
+}
+
+//start
+startApp()
---
+++ message.js
@@ -0,0 +1,240 @@
+barMessage = [
+  {
+    type: "section",
+    text: {
+      type: "mrkdwn",
+      text:"*Die Pickware Bar ist eröffnet!*\n\n\n*official pickware barkeeper ist heute:*"
+    }
+  },
+  {
+    type: "section",
+    text: {
+      type: "mrkdwn",
+      text: `:party_wizard: <@${barkeeper}>`
+    }
+  },
+  {
+    type: "section",
+    text: {
+      type: "mrkdwn",
+      text: "_Alle die noch im Home Office stecken müssen sich Ihre Getränke leider selbst beschaffen._\n_sry mate._\n\n\n*Heute im Angebot:*"
+    }
+  },
+  {
+    type: "divider"
+  },
+  {
+    type: "section",
+    text: {
+      type: "mrkdwn",
+      text:
+        "*Augustiner Lagerbier Hell* :beer: 0,5l\nLeckeres bayrisches Helles. Schöne Farbe , leichte Säure , leicht bitter, Hopfen eher zurückhaltend. Ein Bier das gefallen will, auch gefällt. Trinken kann man das scho guat."
+    },
+    accessory: {
+      type: "button",
+      text: {
+        type: "plain_text",
+        emoji: true,
+        text: "1x bidde"
+      },
+      action_id: "button_1"
+    }
+  },
+  {
+    type: "context",
+    elements: [
+      {
+        type: "mrkdwn",
+        text: ">*Bestellungen:*"
+      }
+    ]
+  },
+  {
+    type: "section",
+    text: {
+      type: "mrkdwn",
+      text:
+        "*Grohe Hell* :beer: 0,33l\nDas Grohe-Hell ist ein solides Bier, welches die für Hell-Biere typischen, mild-süffigen Aromen aufweist. Prädikat: _gut trinkbar_."
+    },
+    accessory: {
+      type: "button",
+      text: {
+        type: "plain_text",
+        emoji: true,
+        text: "1x bidde"
+      },
+      action_id: "button_2"
+    }
+  },
+  {
+    type: "context",
+    elements: [
+      {
+        type: "mrkdwn",
+        text: ">*Bestellungen:*"
+      }
+    ]
+  },
+  {
+    type: "section",
+    text: {
+      type: "mrkdwn",
+      text:
+        "*Grohe Radler* 0,33l\nFür Alle die heute mal langsam starten wollen.\n_\"Gemach! Leicht zum Fallen führt das Eilen.\" -William Shakespeare_"
+    },
+    accessory: {
+      type: "button",
+      text: {
+        type: "plain_text",
+        emoji: true,
+        text: "1x bidde"
+      },
+      action_id: "button_3"
+    }
+  },
+  {
+    type: "context",
+    elements: [
+      {
+        type: "mrkdwn",
+        text: ">*Bestellungen:*"
+      }
+    ]
+  },
+  {
+    type: "section",
+    text: {
+      type: "mrkdwn",
+      text:
+        "*Aperol Spritz* :aperol_spritz: 0.4l\n_sponsored by Aperol Schlomski_ - Die leuchtende Farbe von Aperol Spritz ist unverwechselbar und macht jeden Moment mit deinen Freunden besonders."
+    },
+    accessory: {
+      type: "button",
+      text: {
+        type: "plain_text",
+        emoji: true,
+        text: "1x bidde"
+      },
+      action_id: "button_4"
+    }
+  },
+  {
+    type: "context",
+    elements: [
+      {
+        type: "mrkdwn",
+        text: ">*Bestellungen:*"
+      }
+    ]
+  },
+  {
+    type: "section",
+    text: {
+      type: "mrkdwn",
+      text:
+        "*Melonen Schnaps Shot* :watermelon: 2cl\n_sponsored by Manu_ - Vorsicht Suchtgefahr!"
+    },
+    accessory: {
+      type: "button",
+      text: {
+        type: "plain_text",
+        emoji: true,
+        text: "1x bidde"
+      },
+      action_id: "button_5"
+    }
+  },
+  {
+    type: "context",
+    elements: [
+      {
+        type: "mrkdwn",
+        text: ">*Bestellungen:*"
+      }
+    ]
+  },
+  {
+    type: "section",
+    text: {
+      type: "mrkdwn",
+      text:
+        "*Jägermeister Shot* :jaegermeister: 6cl\nFrisch gezapft im Pckwr Glas! - Genau das Richtige für den dustigen Jägersmann? Am Besten bei -18°C verzehren."
+    },
+    accessory: {
+      type: "button",
+      text: {
+        type: "plain_text",
+        emoji: true,
+        text: "1x bidde"
+      },
+      action_id: "button_6"
+    }
+  },
+  {
+    type: "context",
+    elements: [
+      {
+        type: "mrkdwn",
+        text: ">*Bestellungen:*"
+      }
+    ]
+  },
+  {
+    type: "divider"
+  },
+  {
+    type: "actions",
+    elements: [
+      {
+        type: "button",
+        text: {
+          type: "plain_text",
+          emoji: true,
+          text: "Extrawünsche?"
+        },
+        action_id: "extra_button",
+      }
+    ]
+  },
+  {
+    type: "context",
+    elements: [
+      {
+        type: "mrkdwn",
+        text: " "
+      }
+    ]
+  }
+]
+
+barModal = {
+  type: "modal",
+  callback_id: "view_1",
+  title: {
+    type: "plain_text",
+    text: "Extrawünsche"
+  },
+  blocks: [
+    {
+      type: "input",
+      block_id: "block_1",
+      label: {
+        type: "plain_text",
+        text: "Feeling extra today?"
+      },
+      element: {
+        type: "plain_text_input",
+        placeholder: {
+          type: "plain_text",
+          text: "Getränkewunsch hier eingeben"
+        },
+        action_id: "input_1",
+        multiline: false
+      }
+    }
+  ],
+  submit: {
+    type: "plain_text",
+    text: "Submit"
+  }
+}
